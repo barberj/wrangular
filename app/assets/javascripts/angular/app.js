dcede6410c825e3f8d180905de04870cd66251d3
Justin Barber <barber.justin@gmail.com>
2015-11-28 14:32:21 -0500

    trying to get ui.router to work with inline templates

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,4 +1,14 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }])
   .controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
     $scope.posts = posts.posts;
 
