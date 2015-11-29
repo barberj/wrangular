8ed69653f37ef30d9ad88b4f20e0405e77c9f347
Justin Barber <barber.justin@gmail.com>
2015-11-29 10:21:01 -0500

    style

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -2,44 +2,41 @@ angular.module('flapperNews', [
   'templates',
   'ui.router'
 ]).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-    $stateProvider
-      .state('home', {
-        url: '/home',
-        templateUrl: '/home.html',
-        controller: 'MainCtrl'
-      });
+  $stateProvider.state('home', {
+    url: '/home',
+    templateUrl: '/home.html',
+    controller: 'MainCtrl'
+  });
 
-    $urlRouterProvider.otherwise('home');
-  }])
-  .controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
-    $scope.posts = posts.posts;
+  $urlRouterProvider.otherwise('home');
+}]).controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
+  $scope.posts = posts.posts;
 
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') { return; }
-      if(!$scope.link || $scope.link === '') { return; }
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === ''){ return; }
+    if(!$scope.link || $scope.link === ''){ return; }
 
-      $scope.posts.push({
-        title: $scope.title,
-        upvotes: 0,
-        link: $scope.link
-      });
-      $scope.title = '';
-      $scope.link = '';
-    };
+    $scope.posts.push({
+      title: $scope.title,
+      upvotes: 0,
+      link: $scope.link
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
 
-    $scope.incrementUpvotes = function(post){
-      post.upvotes += 1;
-    };
-  }])
-  .factory('posts', [function(){
-    var o = {
-      posts: [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 15},
-        {title: 'post 4', upvotes: 9},
-        {title: 'post 5', upvotes: 4}
-      ]
-    };
-    return o;
-  }]);
+  $scope.incrementUpvotes = function(post){
+    post.upvotes += 1;
+  };
+}]).factory('posts', [function(){
+  var o = {
+    posts: [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+    ]
+  };
+  return o;
+}]);
